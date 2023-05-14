66c75c5a5247e26286f8082b9ac2fa75a590c4ad
Peter Yockey <peterjyockey@gmail.com>
2023-05-14 12:02:28 -0500

    added more to handlebars, added seeds, login/logout, and routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,11 +1,28 @@
 const router = require('express').Router();
+const  { BlogPost } = require("../models")
 
-router.get('/', (req, res)=>{
-    res.render('all')
+router.get('/', async (req, res)=>{
+    const allPosts = await BlogPost.findAll();
+    const parsedPosts = allPosts.map(post => post.get({plain: true}))
+
+    res.render('all', {
+        posts: parsedPosts,
+        number: 42,
+        bool: false,
+        str: "test"
+    })
 })
 
 router.get('/signup', (req, res)=>{
     res.render('signup')
+})
+
+router.get('/login', (req, res)=>{
+    res.render('login')
+})
+
+router.get('/dashboard', (req, res)=>{
+    res.render('dashboard')
 })
 
 module.exports =  router
