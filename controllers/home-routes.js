c50ae4ee9af394ed12fbf809c869d777f1230de9
Peter Yockey <peterjyockey@gmail.com>
2023-05-15 19:54:58 -0500

    added auth helper, small changes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,24 +1,30 @@
 const router = require('express').Router();
-const  { BlogPost } = require("../models")
+const  { BlogPost, Comment } = require("../models")
 
 router.get('/', async (req, res)=>{
-    const allPosts = await BlogPost.findAll();
+    const allPosts = await BlogPost.findAll({
+        include: [{ model: Comment }],
+    });
+
     const parsedPosts = allPosts.map(post => post.get({plain: true}))
 
     res.render('all', {
-        posts: parsedPosts,
-        number: 42,
-        bool: false,
-        str: "test"
+        parsedPosts,
+        logged_in: req.session.logged_in
     })
-})
-
-router.get('/login', (req, res)=>{
-    res.render('login')
 })
 
 router.get('/dashboard', (req, res)=>{
     res.render('dashboard', {logged_in: req.session.logged_in})
+})
+
+router.get('/login', (req, res)=>{
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+    }
+
+    res.render('login')
 })
 
 module.exports =  router
